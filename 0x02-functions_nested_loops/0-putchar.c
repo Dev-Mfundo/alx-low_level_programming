490c2fef256b803d6c313953f219f5677da6bb54
Dev-Mfundo <DevMfundo@gmail.com>
2023-06-20 15:05:38 -0700

    functions nested

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <unistd.h>
+/**
+* main - writes the character to c stdout
+* Return: on success 0.
+* On error, -1 is returned, and errno is set appropriately
+*/
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
